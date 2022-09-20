80f10d7e2aeef2bf84cba783b081d28f215a3438
venkatesan06 <97516234+venkatesan06@users.noreply.github.com>
2022-09-20 16:43:56 +0530

    Add files via upload

---
+++ maven-jar/src/main/java/com/venkat/DemoMaven/App.java
@@ -0,0 +1,13 @@
+package com.venkat.DemoMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!123" );
+    }
+}
